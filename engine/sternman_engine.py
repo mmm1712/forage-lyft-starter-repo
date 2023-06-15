466ffcb5982c73c9cf4dd3b20e791ec053ff74f6
Marina Zhdanova <77479412+mmm1712@users.noreply.github.com>
2023-06-15 10:18:02 -0700

    Update sternman_engine.py
    These modifications simplify the code by directly returning the boolean expressions instead of using explicit if statements to return boolean values.

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -8,8 +8,6 @@ class SternmanEngine(Car, ABC):
         super().__init__(last_service_date)
         self.warning_light_is_on = warning_light_is_on
 
-    def engine_should_be_serviced(self):
-        if self.warning_light_is_on:
-            return True
-        else:
-            return False
+  def engine_should_be_serviced(self):
+    return self.warning_light_is_on
+
