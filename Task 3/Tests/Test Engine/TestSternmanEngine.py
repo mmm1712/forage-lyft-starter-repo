56e88833ab76b186523735b3f539debceee2983b
Marina Zhdanova <77479412+mmm1712@users.noreply.github.com>
2023-06-30 13:53:37 -0700

    Delete Task 3 directory

--- Task 3/Tests/Test Engine/TestSternmanEngine.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from engine.sternman_engine import SternmanEngine
-
-class TestSternmanEngine(unittest.TestCase):
-    def setUp(self):
-        self.engine = SternmanEngine(True)
-        
-    def test_needs_service(self):
-        self.assertEqual(self.engine.needs_service(), True)
