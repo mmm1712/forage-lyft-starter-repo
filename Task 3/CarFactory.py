8f9b2bf944db6ff56db0bf5044220d8d6b2c489e
Marina Zhdanova <77479412+mmm1712@users.noreply.github.com>
2023-06-30 13:47:17 -0700

    Update CarFactory.py

--- Task 3/CarFactory.py
+++ Task 3/CarFactory.py
@@ -8,36 +8,42 @@ from engine.willoughby_engine import WilloughbyEngine
 
 class CarFactory:
     @staticmethod
-    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage, tire_wear_array):
         engine = CapuletEngine(current_mileage, last_service_mileage)
-        battery = SpindlerBattery(current_date, last_service_date)
-        car = Car(engine,battery)
-        pass
+        battery = SpindlerBattery(last_service_date, current_date)
+        tires = "Carrigan" if any(wear >= 0.9 for wear in tire_wear_array) else "Octoprime"
+        car = Car(engine, battery, tires)
+        return car
 
     @staticmethod
-    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage, tire_wear_array):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
-        battery = SpindlerBattery(current_date, last_service_date)
-        car = Car(engine,battery)
-        pass
+        battery = SpindlerBattery(last_service_date, current_date)
+        tires = "Carrigan" if any(wear >= 0.9 for wear in tire_wear_array) else "Octoprime"
+        car = Car(engine, battery, tires)
+        return car
 
     @staticmethod
-    def create_palindrome(current_date, last_service_date, warning_light_on):
+    def create_palindrome(current_date, last_service_date, warning_light_on, tire_wear_array):
         engine = SternmanEngine(warning_light_on)
-        battery = SpindlerBattery(current_date, last_service_date)
-        car = Car(engine,battery)
-        pass
+        battery = SpindlerBattery(last_service_date, current_date)
+        tires = "Carrigan" if any(wear >= 0.9 for wear in tire_wear_array) else "Octoprime"
+        car = Car(engine, battery, tires)
+        return car
 
     @staticmethod
-    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage, tire_wear_array):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
-        battery = NubbinBattery(current_date, last_service_date)
-        car = Car(engine,battery)
-        pass
+        battery = NubbinBattery(last_service_date, current_date)
+        tires = "Carrigan" if any(wear >= 0.9 for wear in tire_wear_array) else "Octoprime"
+        car = Car(engine, battery, tires)
+        return car
 
     @staticmethod
-    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage, tire_wear_array):
         engine = CapuletEngine(current_mileage, last_service_mileage)
-        battery = NubbinBattery(current_date, last_service_date)
-        car = Car(engine,battery)
-        pass
+        battery = NubbinBattery(last_service_date, current_date)
+        tires = "Carrigan" if any(wear >= 0.9 for wear in tire_wear_array) else "Octoprime"
+        car = Car(engine, battery, tires)
+        return car
+
